76772ec8401269c22e8b27a564ade94f2014e25b
Flavien Solt <flsolt@ethz.ch>
2024-07-01 11:09:46 +0200

    cellift.cc: database vs. get_database()

--- passes/cellift/cellift.cc
+++ passes/cellift/cellift.cc
@@ -756,7 +756,7 @@ struct CelliftPass : public Pass {
 			for (auto cell : module->selected_cells()) {
 				RTLIL::Module *tpl = design->module(cell->type);
 				if (tpl != nullptr) {
-					if (topo_modules.get_database().count(tpl) == 0)
+					if (topo_modules.database.count(tpl) == 0)
 						worklist.push_back(tpl);
 					topo_modules.edge(tpl, module);
 					non_top_modules.insert(cell->type);
